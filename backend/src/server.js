db0d373517f53507e2853288697abfb18e7478c3
Milli-Saved <million12.tenkir@gmail.com>
2023-09-14 11:39:30 +0300

    debug the directory

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ app.use("/api/v1/branch", require("./routes/churchBranchRoutes"));
 app.use("/api/v1/guest", require("./routes/guestMemberRoutes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../../frontend/build")));
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
